b4aa7f5c9a9cac8ad616fce185eb397af816a5e3
chachagof <chacha0932@gmail.com>
2023-07-17 20:24:29 +0800

    feat: initialize api server

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 
 const app = express()
 const PORT = 3000
+const { apis } = require('./routes')
 
+app.use(express.urlencoded({ extended: true })) // body-parser
+app.use('/api', apis)
 app.get('/', (req, res) => {
   res.send('hello world')
 })
