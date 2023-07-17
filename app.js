1bcf262a9676a8afd25c2b3c3b4c72b03a39bac2
chachagof <chacha0932@gmail.com>
2023-07-17 16:55:43 +0800

    feat:add eslint

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-app.get('/',(req.res) => {
+app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-app.listen(PORT,() => {
+app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
