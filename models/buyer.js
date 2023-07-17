be4748f81cc6448f2f157460d79d868c306424eb
chachagof <chacha0932@gmail.com>
2023-07-17 18:10:23 +0800

    feat:add associate between buyer and cart

--- models/buyer.js
+++ models/buyer.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate (models) {
       // define association here
+      Buyer.hasOne(models.Cart, { foreignKey: 'CartId' })
     }
   }
   Buyer.init({
--- models/cart.js
+++ models/cart.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class cart extends Model {
+  class Cart extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,15 +11,16 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate (models) {
       // define association here
+      Cart.belongsTo(models.Buyer, { foreignKey: 'buyerId' })
     }
   }
-  cart.init({
+  Cart.init({
     buyerId: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'cart',
+    modelName: 'Cart',
     tableName: 'Carts',
     underscored: true
   })
-  return cart
+  return Cart
 }
