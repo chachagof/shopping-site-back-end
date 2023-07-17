be037921d3d0c283363660ff0d40f07c7f76e9d6
chachagof <chacha0932@gmail.com>
2023-07-17 18:07:29 +0800

    feat:add cart-commodity model

---
+++ models/cart-commodity.js
@@ -0,0 +1,26 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class CartCommodity extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  }
+  CartCommodity.init({
+    commodityId: DataTypes.INTEGER,
+    cartId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'CartCommodity',
+    tableName: 'CartCommodities',
+    underscored: true
+  })
+  return CartCommodity
+}
