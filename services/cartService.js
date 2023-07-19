b24e96e5ff2a28c0b3d22bd3ae361eb9030be925
chachagof <chacha0932@gmail.com>
2023-07-19 20:31:57 +0800

    feat:redesign addToCart api 1.when first call this api will create new one 2.Afterwards,his data amount will plus 1

--- services/cartService.js
+++ services/cartService.js
@@ -4,10 +4,23 @@ const cartService = {
   addToCart: (req, cb) => {
     const commodityId = req.params.commodityId
     const cartId = req.user.Cart.id
-    CartCommodity.create({
-      commodityId,
-      cartId
+    const { amount } = req.body
+    CartCommodity.findOne({
+      where: {
+        commodityId,
+        cartId
+      }
     })
+      .then(cartcommodity => {
+        if (!cartcommodity) {
+          return CartCommodity.create({
+            amount,
+            commodityId,
+            cartId
+          })
+        }
+        return cartcommodity.increment('amount', { by: 1 })
+      })
       .then(() => cb(null, { status: 200, message: '成功加入購物車' }))
       .catch(err => cb(err))
   },
