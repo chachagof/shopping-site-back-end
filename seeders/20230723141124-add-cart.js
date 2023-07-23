d2a6ee5fd76a26b84c064a52d36f43660449fa28
chachagof <chacha0932@gmail.com>
2023-07-23 22:25:00 +0800

    feat:add cart seeder

---
+++ seeders/20230723141124-add-cart.js
@@ -0,0 +1,22 @@
+'use strict'
+const { Buyer } = require('../models')
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const buyerCart = await Buyer.findAll({
+      raw: true
+    })
+    const cart = buyerCart.map(data => {
+      return {
+        buyer_id: data.id,
+        created_at: data.createdAt,
+        updated_at: data.updatedAt
+      }
+    })
+    await queryInterface.bulkInsert('Carts', cart, {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Carts', null, {})
+  }
+}
